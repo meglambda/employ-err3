8c65c1d610506ac16715433554f1a02d3a96fc44
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-18 16:35:38 -0700

    try again

--- server/server.js
+++ server/server.js
@@ -4,28 +4,51 @@ import bodyParser from 'body-parser'
 import express from 'express'
 import path from 'path'
 
-const PORT = process.env.PORT || 8080;
 const app = express()
+const router = express.Router()
+
+const PORT = process.env.PORT || 8080;
+
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(staticFiles)
 
-// const staticFiles = express.static(path.join(__dirname, '../../client/build'))
-app.use(express.static(path.join(__dirname, '../../client/build')));
 
-// app.use(staticFiles)
-
-app.get('/api/employees', (req, res) => {
+router.get('/api/employees', (req, res) => {
 	res.send(employees);
 });
 
-app.get('/api/employers', (req, res) => {
+router.get('/api/employers', (req, res) => {
 	res.send(employers);
 });
 
-// any routes not picked up by the server api will be handled by the react router
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-});
 
-app.listen(PORT, () => console.log(`App listening on ${PORT}`));
+app.use(router)
+
+// any routes not picked up by the server api will be handled by the react router
+app.use('/*', staticFiles)
+
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
+
+
+
+// app.use(express.static(path.join(__dirname, '../../client/build')));
+
+// app.get('/api/employees', (req, res) => {
+// 	res.send(employees);
+// });
+
+// app.get('/api/employers', (req, res) => {
+// 	res.send(employers);
+// });
+
+// // any routes not picked up by the server api will be handled by the react router
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+// });
+
+// app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
