e55a7dee2b5f2da2734be9257b2cca229652d360
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-18 16:29:44 -0700

    try again

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.get('/api/employers', (req, res) => {
 
 // any routes not picked up by the server api will be handled by the react router
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`App listening on ${PORT}`));
