6350f18c8200eaa773d13cf919b5e6574e83a2ab
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-19 10:31:11 -0700

    initial files for mongo/mongoose setup

---
+++ server/routes/apiRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import Employees from '../models/employees.js';
+import Employers from '../models/employers.js';
+
+const router = express.Router();
+
+
+router.get('/api/employees', (req, res) => {
+	Employees.find({})
+		.then((results) => {
+			res.send(results);
+		})
+		.catch((err) => { 
+			console.log(err.message);
+			res.status(500).send(err.message ? err.message : "Internal server blowup");
+		})
+});
+
+router.get('/api/employers', (req, res) => {
+	Employers.find({})
+		.then((results) => {
+			res.send(results);
+		})
+		.catch((err) => { 
+			console.log(err.message);
+			res.status(500).send(err.message ? err.message : "Internal server blowup");
+		})
+});
+
+
+module.exports = router;
