901ebab0a7646313362b195d1a396b30e4fd0651
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-18 20:13:19 -0700

    modified notfound

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -7,7 +7,6 @@ function NotFound() {
 		<div className="jumbotron text-center" style={{height: "100vh"}}>
 			<h1 className="display-3">404 Not Found</h1>
 			<p className="lead">How did you get here?</p>
-			<br />
 			<a className="btn btn-default" href='/' style={{border: '1px solid black'}}>Go back to your home...</a>
 		</div>
 	)
